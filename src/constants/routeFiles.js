955ca8f715b5059a4382ac7ad669dc1fc576ee9c
Rodrigo Andrés Góngora Herrera <andres11362@gmail.com>
2022-02-04 22:45:07 -0500

    Se ajustan rutas para deploy en produccion

--- src/constants/routeFiles.js
+++ src/constants/routeFiles.js
@@ -2,4 +2,4 @@
  * Ruta de los archivos de la aplicación
  */
 // eslint-disable-next-line no-restricted-globals
-export const routeFiles = `http://localhost/prueba-el-tiempo_back/storage/app/`;
+export const routeFiles = `https://prueba-eltiempo.herokuapp.com/storage/app/`;
